7e495e5d05698b5904505705cc1c42f09ada4108
Matt Chan <matt.chan@autodesk.com>
2018-09-03 15:58:44 -0700

    time methods

--- 1_technical_problem_solving_and_analysis/problems/1_sum_of_n.rb
+++ 1_technical_problem_solving_and_analysis/problems/1_sum_of_n.rb
@@ -1,6 +1,13 @@
 # Problem:
 # Calculate the sum of the first n numbers. Method should take n parameter and return a sum of the numbers
 
+def time_method(method, arg)
+  beginning_time = Time.now
+  self.send(method, arg)
+  end_time = Time.now
+  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
+end
+
 def sum_of(n)
   total = 0
   while n > 0
@@ -12,3 +19,9 @@ end
 
 puts sum_of(3) == 6
 puts sum_of(4) == 10
+
+time_method(:sum_of, 1)
+time_method(:sum_of, 10)
+time_method(:sum_of, 100)
+time_method(:sum_of, 1000)
+time_method(:sum_of, 10000)
