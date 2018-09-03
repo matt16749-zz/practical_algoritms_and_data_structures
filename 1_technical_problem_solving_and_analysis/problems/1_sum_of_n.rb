9f12cec3eb2897ea68e7cfa95e56de26fa78b574
Matt Chan <matt.chan@autodesk.com>
2018-09-03 16:26:23 -0700

    adds solutions to both problems

--- 1_technical_problem_solving_and_analysis/problems/1_sum_of_n.rb
+++ 1_technical_problem_solving_and_analysis/problems/1_sum_of_n.rb
@@ -25,3 +25,34 @@ time_method(:sum_of, 10)
 time_method(:sum_of, 100)
 time_method(:sum_of, 1000)
 time_method(:sum_of, 10000)
+=begin
+  #sum_of is not efficient. O(n). There is a faster way without looping.
+  Hint: Try summing numbers 1 - 6 by grouping 1 and 6, 2 and 5, 3 and 4. Notice there are 3 pairs each of which total 7.
+=end
+
+def faster_sum_of(n, total=0)
+  if n > 0
+    if n.even?
+      total = (n / 2) * (n + 1)
+    else
+      total = n * (n / 2.to_f).ceil
+    end
+  end
+  total
+end
+
+puts faster_sum_of(6) == 21
+puts faster_sum_of(4) == 10
+puts faster_sum_of(2) == 3
+puts faster_sum_of(7) == 28
+puts faster_sum_of(5) == 15
+puts faster_sum_of(3) == 6
+
+time_method(:faster_sum_of, 1)
+time_method(:faster_sum_of, 10)
+time_method(:faster_sum_of, 100)
+time_method(:faster_sum_of, 1000)
+time_method(:faster_sum_of, 10000)
+=begin
+  #faster_sum_of is O(1)
+=end
